fac30eed9da7ab9930fadb9421eef71262c64472
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-10-25 15:38:26 -0300

    Remove django default tables from erd
    See #1

--- scripts/erd.py
+++ scripts/erd.py
@@ -10,6 +10,19 @@ metadata.reflect(engine)
 table_names = list(metadata.tables.keys())
 table_names.reverse()
 package = Package(name='teste')
+django_default_tables = [
+    'django_session',
+    'django_migrations',
+    'django_admin_log',
+    'auth_user_user_permissions',
+    'auth_user_groups',
+    'auth_user',
+    'django_content_type',
+    'auth_permission',
+    'auth_group_permissions',
+    'auth_group',
+]
+table_names = list(set(table_names) - set(django_default_tables))
 
 for name in table_names:
     control = formats.SqlControl(table=name, basepath='data')
