57768acab7477039e2a76777009052692fb78f38
Gabriel Dornas <gabrielbdornas@gmail.com>
2024-10-25 18:20:49 -0300

    Create Expense Owern class
    See #6
    And table
    Other fixes

--- finance/models.py
+++ finance/models.py
@@ -1,7 +1,18 @@
 from django.db import models
-from django.core.validators import MinValueValidator
+from django.core.validators import (
+    MinValueValidator,
+    MaxValueValidator,
+)
 
 class Category(models.Model):
+    name = models.CharField(max_length=100)
+    start_date = models.DateField(auto_now_add=False)
+    end_date = models.DateField(null = True, blank = True)
+
+    def __str__(self):
+        return self.name
+
+class Owner(models.Model):
     name = models.CharField(max_length=100)
     start_date = models.DateField(auto_now_add=False)
     end_date = models.DateField(null = True, blank = True)
@@ -12,7 +23,8 @@ class Category(models.Model):
 class Expense(models.Model):
     name = models.CharField(max_length=100)
     category = models.ForeignKey(Category, on_delete=models.RESTRICT)
-    value = models.FloatField(validators=[MinValueValidator(0.01)])
+    owners = models.ManyToManyField(Owner, through='ExpenseOwner')
+    estimated_value = models.FloatField(validators=[MinValueValidator(0.01)])
     start_date = models.DateField(auto_now_add=False)
     end_date = models.DateField(null = True, blank = True)
 
@@ -21,3 +33,19 @@ class Expense(models.Model):
 
     def __str__(self):
         return self.name
+
+class ExpenseOwner(models.Model):
+    expense = models.ForeignKey(Expense, on_delete=models.RESTRICT)
+    owner = models.ForeignKey(Owner, on_delete=models.RESTRICT)
+    percentage = models.FloatField(
+        validators=[
+            MinValueValidator(0.01),
+            MaxValueValidator(100.00),
+            ]
+        )
+
+    class Meta:
+        db_table = 'expense_owners'
+
+    def __str__(self):
+        return f'{self.expense} - {self.owner} - {self.percentage}'
